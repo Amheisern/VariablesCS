ca3bdfd8b58b7fa1dbce11661156618da1490400
lodashreload <icarusjapan@gmail.com>
2021-11-11 23:55:07 -0500

    stage 1 done and stage 2 started

--- Program.cs
+++ Program.cs
@@ -2,11 +2,37 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            /* Problem: Create a variable (use your best judgment for type) 
+            that stores the numberOfCupsOfCoffee that you drink every day.
+            Number of cups of Coffee over the course of a week. 
+              Examples: monday = 2 tuesday = 6 wednesday = 2 thursday = 3 friday = 4
+              Data: there are two variables 1. the numberOfCupsOfCoffee 2. day of the week 2.
+              Algorithm: for each day record the numberOfCupsOfCoffee based of the day of the week */
+
+
+            Console.WriteLine("How many cups of coffee have you had today ?");
+            //Recording the number of cups of coffee on a given day
+            var numberOfCupsOfCoffee = 3;
+            var fullname = " Nicholas Amheiser ";
+            var today = DateTime.Now;
+            Console.WriteLine(numberOfCupsOfCoffee + fullname + today);
+
+            // Type your username and press enter
+            Console.Write("Enter username:");
+            var username = Console.ReadLine();
+            Console.WriteLine(" Greetings " + username);
+
+            //Asking user for input
+            Console.Write("Enter two numbers: ");
+            var numbers = Console.ReadLine();
+
+
+
+
+        }
     }
-  }
 }
