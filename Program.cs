559be885837b624a42e6983c1d8da46c20a49baf
lodashreload <icarusjapan@gmail.com>
2021-11-12 23:06:42 -0500

    Variables mostly finished

--- Program.cs
+++ Program.cs
@@ -24,11 +24,40 @@ namespace VariablesCS
             // Type your username and press enter
             Console.Write("Enter username:");
             var username = Console.ReadLine();
+            if (username == "alice")
+            {
+                Console.Write("Welcome to the looking glass Alice!");
+            }
             Console.WriteLine(" Greetings " + username);
 
             //Asking user for input
-            Console.Write("Enter two numbers: ");
-            var numbers = Console.ReadLine();
+            //I'm not sure I have done with right.  So far this was by far the hardest part
+            Console.Write("Enter a number: ");
+            string firstNumberAsString = Console.ReadLine();
+            double firstOperand = Convert.ToDouble(firstNumberAsString);
+
+            Console.Write("Write a second number: ");
+            string secondNumberAsString = Console.ReadLine();
+            double secondOperand = Convert.ToDouble(secondNumberAsString);
+
+            //Doing Math
+            var sum = firstOperand + secondOperand;
+            var difference = secondOperand - firstOperand;
+            var product = firstOperand * secondOperand;
+            var quotient = firstOperand / secondOperand;
+            var remainder = secondOperand % secondOperand;
+            Console.WriteLine($"These are your lotto numbers: {sum} {difference} {product} {quotient} {remainder}");
+
+
+
+
+            //double.Parse(firstNumberAsString + secondNumberAsString);
+            // var firstOperand = double.Parse(firstNumberAsString);
+            // var secondOperand = double.Parse(secondNumberAsString);
+            //Console.WriteLine(Convert.ToDoublefirstNumberAsString)
+
+
+
 
 
 
